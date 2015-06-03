3b2c2710e5c94516ed3d11bfc6902d9ec0c12a48
sunkaixuan <610262374@qq.com>
2015-06-03 14:48:12 +0800

    IsWhat

--- SyntacticSugar/Extenions/IsWhat.cs
+++ SyntacticSugar/Extenions/IsWhat.cs
@@ -18,183 +18,183 @@ namespace SyntacticSugar
         /// <summary>
         /// 值在的范围？
         /// </summary>
-        /// <param name="o"></param>
+        /// <param name="thisValue"></param>
         /// <param name="begin">大于等于begin</param>
         /// <param name="end">小于等于end</param>
         /// <returns></returns>
-        public static bool IsInRange(this int o, int begin, int end)
+        public static bool IsInRange(this int thisValue, int begin, int end)
         {
-            return o >= begin && o <= end;
+            return thisValue >= begin && thisValue <= end;
         }
         /// <summary>
         /// 值在的范围？
         /// </summary>
-        /// <param name="o"></param>
+        /// <param name="thisValue"></param>
         /// <param name="begin">大于等于begin</param>
         /// <param name="end">小于等于end</param>
         /// <returns></returns>
-        public static bool IsInRange(this DateTime o, DateTime begin, DateTime end)
+        public static bool IsInRange(this DateTime thisValue, DateTime begin, DateTime end)
         {
-            return o >= begin && o <= end;
+            return thisValue >= begin && thisValue <= end;
         }
 
         /// <summary>
         /// 在里面吗?
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="o"></param>
+        /// <param name="thisValue"></param>
         /// <param name="values"></param>
         /// <returns></returns>
-        public static bool IsIn<T>(this T o, params T[] values)
+        public static bool IsIn<T>(this T thisValue, params T[] values)
         {
-            return values.Contains(o);
+            return values.Contains(thisValue);
         }
 
         /// <summary>
         /// 是null或""?
         /// </summary>
         /// <returns></returns>
-        public static bool IsNullOrEmpty(this object o)
+        public static bool IsNullOrEmpty(this object thisValue)
         {
-            if (o == null || o == DBNull.Value) return true;
-            return o.ToString() == "";
+            if (thisValue == null || thisValue == DBNull.Value) return true;
+            return thisValue.ToString() == "";
         }
         /// <summary>
         /// 是null或""?
         /// </summary>
         /// <returns></returns>
-        public static bool IsNullOrEmpty(this Guid? o)
+        public static bool IsNullOrEmpty(this Guid? thisValue)
         {
-            if (o == null) return true;
-            return o == Guid.Empty;
+            if (thisValue == null) return true;
+            return thisValue == Guid.Empty;
         }
         /// <summary>
         /// 是null或""?
         /// </summary>
         /// <returns></returns>
-        public static bool IsNullOrEmpty(this Guid o)
+        public static bool IsNullOrEmpty(this Guid thisValue)
         {
-            if (o == null) return true;
-            return o == Guid.Empty;
+            if (thisValue == null) return true;
+            return thisValue == Guid.Empty;
         }
 
         /// <summary>
         /// 有值?(与IsNullOrEmpty相反)
         /// </summary>
         /// <returns></returns>
-        public static bool IsValuable(this object o)
+        public static bool IsValuable(this object thisValue)
         {
-            if (o == null) return false;
-            return o.ToString() != "";
+            if (thisValue == null) return false;
+            return thisValue.ToString() != "";
         }
         /// <summary>
         /// 有值?(与IsNullOrEmpty相反)
         /// </summary>
         /// <returns></returns>
-        public static bool IsValuable(this IEnumerable<object> o)
+        public static bool IsValuable(this IEnumerable<object> thisValue)
         {
-            if (o == null || o.Count() == 0) return false;
+            if (thisValue == null || thisValue.Count() == 0) return false;
             return true;
         }
 
         /// <summary>
         /// 是零?
         /// </summary>
-        /// <param name="o"></param>
+        /// <param name="thisValue"></param>
         /// <returns></returns>
-        public static bool IsZero(this object o)
+        public static bool IsZero(this object thisValue)
         {
-            return (o == null || o.ToString() == "0");
+            return (thisValue == null || thisValue.ToString() == "0");
         }
 
         /// <summary>
         /// 是INT?
         /// </summary>
-        /// <param name="o"></param>
+        /// <param name="thisValue"></param>
         /// <returns></returns>
-        public static bool IsInt(this object o)
+        public static bool IsInt(this object thisValue)
         {
-            if (o == null) return false;
-            return Regex.IsMatch(o.ToString(), @"^\d+$");
+            if (thisValue == null) return false;
+            return Regex.IsMatch(thisValue.ToString(), @"^\d+$");
         }
         /// <summary>
         /// 不是INT?
         /// </summary>
-        /// <param name="o"></param>
+        /// <param name="thisValue"></param>
         /// <returns></returns>
-        public static bool IsNoInt(this object o)
+        public static bool IsNoInt(this object thisValue)
         {
-            if (o == null) return true;
-            return !Regex.IsMatch(o.ToString(), @"^\d+$");
+            if (thisValue == null) return true;
+            return !Regex.IsMatch(thisValue.ToString(), @"^\d+$");
         }
 
         /// <summary>
         /// 是金钱?
         /// </summary>
-        /// <param name="o"></param>
+        /// <param name="thisValue"></param>
         /// <returns></returns>
-        public static bool IsMoney(this object o)
+        public static bool IsMoney(this object thisValue)
         {
-            if (o == null) return false;
+            if (thisValue == null) return false;
             double outValue = 0;
-            return double.TryParse(o.ToString(), out outValue);
+            return double.TryParse(thisValue.ToString(), out outValue);
         }
 
         /// <summary>
         /// 是邮箱?
         /// </summary>
-        /// <param name="o"></param>
+        /// <param name="thisValue"></param>
         /// <returns></returns>
-        public static bool IsEamil(this object o)
+        public static bool IsEamil(this object thisValue)
         {
-            if (o == null) return false;
-            return Regex.IsMatch(o.ToString(), @"^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$");
+            if (thisValue == null) return false;
+            return Regex.IsMatch(thisValue.ToString(), @"^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$");
         }
 
         /// <summary>
         /// 是手机?
         /// </summary>
-        /// <param name="o"></param>
+        /// <param name="thisValue"></param>
         /// <returns></returns>
-        public static bool IsMobile(this object o)
+        public static bool IsMobile(this object thisValue)
         {
-            if (o == null) return false;
-            return Regex.IsMatch(o.ToString(), @"^\d{11}$");
+            if (thisValue == null) return false;
+            return Regex.IsMatch(thisValue.ToString(), @"^\d{11}$");
         }
 
         /// <summary>
         /// 是座机?
         /// </summary>
-        public static bool IsTelephone(this object o)
+        public static bool IsTelephone(this object thisValue)
         {
-            if (o == null) return false;
-            return System.Text.RegularExpressions.Regex.IsMatch(o.ToString(), @"^(\(\d{3,4}\)|\d{3,4}-|\s)?\d{8}$");
+            if (thisValue == null) return false;
+            return System.Text.RegularExpressions.Regex.IsMatch(thisValue.ToString(), @"^(\(\d{3,4}\)|\d{3,4}-|\s)?\d{8}$");
 
         }
 
         /// <summary>
         /// 是身份证?
         /// </summary>
-        /// <param name="o"></param>
+        /// <param name="thisValue"></param>
         /// <returns></returns>
-        public static bool IsIDcard(this object o)
+        public static bool IsIDcard(this object thisValue)
         {
-            if (o == null) return false;
-            return System.Text.RegularExpressions.Regex.IsMatch(o.ToString(), @"^(\d{15}$|^\d{18}$|^\d{17}(\d|X|x))$");
+            if (thisValue == null) return false;
+            return System.Text.RegularExpressions.Regex.IsMatch(thisValue.ToString(), @"^(\d{15}$|^\d{18}$|^\d{17}(\d|X|x))$");
         }
 
         /// <summary>
         ///是适合正则匹配?
         /// </summary>
-        /// <param name="o"></param>
+        /// <param name="thisValue"></param>
         /// <param name="begin">大于等于begin</param>
         /// <param name="end">小于等于end</param>
         /// <returns></returns>
-        public static bool IsMatch(this object o, string pattern)
+        public static bool IsMatch(this object thisValue, string pattern)
         {
-            if (o == null) return false;
+            if (thisValue == null) return false;
             Regex reg = new Regex(pattern);
-            return reg.IsMatch(o.ToString());
+            return reg.IsMatch(thisValue.ToString());
         }
     }
 }
