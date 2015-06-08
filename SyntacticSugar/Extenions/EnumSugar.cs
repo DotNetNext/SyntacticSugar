8b1641fc4de521cb66e621df83523e5b3074c3d0
sunkaixuan <610262374@qq.com>
2015-06-08 09:32:48 +0800

    BUG

---
+++ SyntacticSugar/Extenions/EnumSugar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.ComponentModel;
+namespace SyntacticSugar.Extenions
+{
+    /// <summary>
+    /// 枚举帮助类
+    /// </summary>
+    public static class EnumSugar
+    {
+
+    }
+}
---
+++ Test/Extenions/EnumTest.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SyntacticSugar;
+namespace Test.Extenions
+{
+    public partial class EnumTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public enum estType
+        {
+            value1 = 1,
+            value2 = 2
+        }
+    }
+}
