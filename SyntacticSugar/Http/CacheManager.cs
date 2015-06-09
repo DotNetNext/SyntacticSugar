b9987fdbf6e9d9cc9a9430670e3b1c957042da97
sunkaixuan <610262374@qq.com>
2015-06-09 14:39:03 +0800

    CacheManager

--- SyntacticSugar/Http/CacheManager.cs
+++ SyntacticSugar/Http/CacheManager.cs
@@ -14,7 +14,7 @@ namespace SyntacticSugar
     /// ** 创始时间：2015-6-9
     /// ** 修改时间：-
     /// ** 作者：sunkaixuan
-    /// ** 使用说明：
+    /// ** 使用说明：http://www.cnblogs.com/sunkaixuan/p/4563462.html
     /// </summary>
     /// <typeparam name="K">键</typeparam>
     /// <typeparam name="V">值</typeparam>
