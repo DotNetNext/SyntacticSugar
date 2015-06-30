593bf46b7afda8d4b9a63b96b000b0e568cdede8
sunkaixuan <610262374@qq.com>
2015-06-30 14:12:47 +0800

    FileSugar

--- SyntacticSugar/IO/FileSugar.cs
+++ SyntacticSugar/IO/FileSugar.cs
@@ -20,6 +20,59 @@ namespace SyntacticSugar
     /// </summary>
     public class FileSugar
     {
+        #region 获取文件路并自动创建目录
+        /// <summary>
+        /// 根据文件目录、编号、文件名生成文件路径，并且创建文件存放目录
+        /// 格式为:/directory/code/filename
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="directory"></param>
+        /// <param name="code"></param>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public string GetFiePathAndCreateDirectoryByCode<T>(string directory, T code, string fileName)
+        {
+            if (directory == null)
+            {
+                throw new ArgumentNullException("FileSugar.GetCreatePath.directory");
+            }
+            directory = directory.TrimEnd('/');
+            string path = new StringBuilder("{0}//{1}//{2}").AppendFormat(directory, code, fileName).ToString();
+            directory = Path.GetDirectoryName(path);
+            if (!IsExistDirectory(directory))
+            {
+                CreateDirectory(directory);
+            }
+            return path;
+
+        }
+        /// <summary>
+        /// 根据文件目录、日期、文件名生成文件路径，并且创建文件存放目录
+        /// 格式为:/directory/2015/01/01/filename
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="directory"></param>
+        /// <param name="code"></param>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public string GetFiePathAndCreateDirectoryByDate<T>(string directory, string fileName)
+        {
+            if (directory == null)
+            {
+                throw new ArgumentNullException("FileSugar.GetCreatePath.directory");
+            }
+            directory = directory.TrimEnd('/');
+            string path = new StringBuilder("{0}//{1}//{2}//{3}//{4}").AppendFormat(directory, DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, fileName).ToString();
+            directory = Path.GetDirectoryName(path);
+            if (!IsExistDirectory(directory))
+            {
+                CreateDirectory(directory);
+            }
+            return path;
+
+        } 
+        #endregion
+
         #region 获得当前绝对路径
         /// <summary>
         /// 获得当前绝对路径
