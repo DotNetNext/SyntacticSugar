a51cba2715e1dbf453d3076453f0e58d6e353dc4
sunkaixuan <610262374@qq.com>
2015-07-10 12:36:53 +0800

    ReflectionSugar

---
+++ Test/IO/ReflectionTest.aspx.cs
@@ -0,0 +1,19 @@
+﻿using SyntacticSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Test.IO
+{
+    public partial class ReflectionTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           var f=  ReflectionSugar.CreateInstance<FileSugar>("SyntacticSugar.FileSugar", "SyntacticSugarx");
+         
+        }
+    }
+}
