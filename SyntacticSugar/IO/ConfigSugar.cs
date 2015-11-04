abe77305662d33963b028a9ed87113e7e2e0bbc4
sunkaixuan <sunkaixuan>
2015-11-04 17:26:25 +0800

    update ConfigSugar

--- SyntacticSugar/IO/ConfigSugar.cs
+++ SyntacticSugar/IO/ConfigSugar.cs
@@ -14,7 +14,7 @@ namespace SyntacticSugar
     public class ConfigSugar
     {
         /// <summary>
-        /// 获得Web.config里的配置信息
+        /// 获得Web.config ConnectionString 里的配置信息
         /// </summary>
         /// <param name="key"></param>
         /// <returns></returns>
@@ -22,13 +22,24 @@ namespace SyntacticSugar
         {
             return ConfigurationManager.ConnectionStrings[key].ToString();
         }
+
+        /// <summary>
+        /// 获得Web.config ConnectionString 里的配置信息
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string GetConnectionString(string key)
+        {
+            return ConfigurationManager.ConnectionStrings[key].ToString();
+        }
+
         /// <summary>
         /// 将配置信息转化为字符串
         /// </summary>
         /// <param name="key">AppSettings中的key</param>
         /// <param name="defaultValue">默认返回值</param>
         /// <returns>找到与key相应的值，则返回该值，否则返回默认值</returns>
-        public static string GetAppString(string key, string defaultValue)
+        public static string GetAppString(string key, string defaultValue=null)
         {
             string keyValue = ConfigurationManager.AppSettings[key];
             if (!string.IsNullOrEmpty(keyValue))
@@ -44,7 +55,7 @@ namespace SyntacticSugar
         /// <param name="key">AppSettings中的key</param>
         /// <param name="defaultValue">默认返回值</param
         /// <returns>找到与key相应的值，则返回该值，否则返回默认值</returns>
-        public static int GetAppInt(string key, int defaultValue)
+        public static int GetAppInt(string key, int defaultValue=0)
         {
             string keyValue = ConfigurationManager.AppSettings[key];
             int tempValue;
@@ -61,7 +72,7 @@ namespace SyntacticSugar
         /// <param name="key">AppSettings中的key</param>
         /// <param name="defaultValue">默认返回值</param
         /// <returns>找到与key相应的值，则返回该值，否则返回默认值</returns>
-        public static bool GetAppBool(string key, bool defaultValue)
+        public static bool GetAppBool(string key, bool defaultValue=false)
         {
             string keyValue = ConfigurationManager.AppSettings[key];
             bool tempValue;
