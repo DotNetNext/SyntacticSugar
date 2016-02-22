827428a73ee9d32c13cd4be215931ca67427df3a
sunkaixuan <sunkaixuan>
2016-02-22 16:56:23 +0800

    OK

---
+++ SyntacticSugar/DataType/DataTableType/DataTableExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Web.Script.Serialization;
+
+namespace SyntacticSugar
+{
+    public static class DataTableExtensions
+    {
+        
+        public static  string DataTableToJson(this DataTable table)
+        {
+            JavaScriptSerializer jsSerializer = new JavaScriptSerializer();
+            List<Dictionary<string, object>> parentRow = new List<Dictionary<string, object>>();
+            Dictionary<string, object> childRow;
+            foreach (DataRow row in table.Rows)
+            {
+                childRow = new Dictionary<string, object>();
+                foreach (DataColumn col in table.Columns)
+                {
+                    childRow.Add(col.ColumnName, row[col]);
+                }
+                parentRow.Add(childRow);
+            }
+            return jsSerializer.Serialize(parentRow);
+        }
+        //*
+    }
+     
+}
