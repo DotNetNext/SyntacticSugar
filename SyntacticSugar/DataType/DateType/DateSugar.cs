4e8ea8279fc2e99eb2e3d9074be83840cd5a0d19
sunkaixuan <610262374@qq.com>
2016-08-29 12:42:01 +0800

    ok

--- SyntacticSugar/DataType/DateType/DateSugar.cs
+++ SyntacticSugar/DataType/DateType/DateSugar.cs
@@ -70,10 +70,39 @@ namespace SyntacticSugar
                 return 0;
             }
         }
+        /// <summary>
+        /// 判段两个时间是否是同一天
+        /// </summary>
+        /// <param name="d1"></param>
+        /// <param name="d2"></param>
+        /// <returns></returns>
+        public static bool IsSameDay(DateTime? d1, DateTime? d2)
+        {
+            if (d1 == null)
+            {
+                d1 = DateTime.MinValue;
+            }
+            if (d2 == null)
+            {
+                d2 = DateTime.MinValue;
+            }
+            return
+                IsSameDay(Convert.ToDateTime(d1), Convert.ToDateTime(d2));
+        }
+        /// <summary>
+        /// 判段两个时间是否是同一天
+        /// </summary>
+        /// <param name="d1"></param>
+        /// <param name="d2"></param>
+        /// <returns></returns>
+        public static bool IsSameDay(DateTime d1, DateTime d2)
+        {
+            return d1.ToString("yyyy-MM-dd")==d2.ToString("yyyy-MM-dd");
+        }
 
 
         /// <summary>
-        /// 近回时间之差
+        /// 返回时间之差
         /// </summary>
         /// <param name="interval"></param>
         /// <param name="date1"></param>
@@ -101,6 +130,44 @@ namespace SyntacticSugar
                 default:
                     return Fix(ts.TotalSeconds);
             }
+        }
+        /// <summary>
+        /// 取时间的交集部分的分钟数
+        /// </summary>
+        /// <param name="minT">minT为交集的最小时</param>
+        /// <param name="maxT">maxT为交集的最大时间</param>
+        /// <param name="t1">时间1</param>
+        /// <param name="t2">时间2</param>
+        /// <returns></returns>
+        public static int GetTwoTimeAreaIntersection(DateTime minT, DateTime maxT, DateTime t1, DateTime t2)
+        {
+            var range1 = new { start = minT, end = maxT };
+            var range2 = new { start = t1, end = t2 };
+            var iStart = range1.start < range2.start ? range2.start : range1.start;
+            var iEnd = range1.end < range2.end ? range1.end : range2.end;
+            var newRange = iStart < iEnd ? new { start = iStart, end = iEnd } : null;
+            if (newRange == null) return 0;
+            //返回区间不能超出请假单范围
+            DateTime resBegin = DateTime.Now;
+            DateTime resEnd = DateTime.Now;
+            if (newRange.end > maxT)
+            {
+                resEnd = maxT;
+            }
+            else
+            {
+                resEnd = newRange.end;
+            }
+            if (newRange.start < minT)
+            {
+                resBegin = minT;
+            }
+            else
+            {
+                resBegin = newRange.start;
+            }
+
+            return Convert.ToInt32((resEnd - resBegin).TotalMinutes);
         }
         private static long Fix(double Number)
         {
--- Test/DateType/DateTypeTest.aspx.cs
+++ Test/DateType/DateTypeTest.aspx.cs
@@ -15,6 +15,24 @@ namespace Test.DateType
            var res60= DateSugar.DateDiff(DateSugar.DateInterval.Second,"2016-1-1 11:12:00".TryToDate(),"2015-1-1 11:13:00".TryToDate());
 
            var res2 = DateSugar.ComparisonTime("2015-1-1 18:13:02".TryToDate(), "2015-1-1 11:13:00".TryToDate(), "2015-1-1 4:14:01".TryToDate());
+
+
+           //两个时间是否是同一天
+           var isSameDay = DateSugar.IsSameDay(DateTime.Now,DateTime.Now.AddDays(-1));
+
+           var isSameDay2 = DateSugar.IsSameDay(DateTime.Now, DateTime.Now);
+
+           var isSameDay3 = DateSugar.IsSameDay(DateTime.Now.AddDays(-50), DateTime.Now);
+
+           var isSameDay4 = DateSugar.IsSameDay((DateTime?)DateTime.Now,(DateTime?)DateTime.Now);
+
+
+           var intMinutes = DateSugar.GetTwoTimeAreaIntersection(
+               "2014-1-1 12:00:00".TryToDate(),
+               "2014-1-1 12:30:00".TryToDate(),
+               "2014-1-1 12:15:00".TryToDate(),
+               "2014-1-1 13:00:00".TryToDate());
+        
         }
     }
 }
