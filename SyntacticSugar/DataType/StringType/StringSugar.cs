a3d7a0a09a46f45c3030fb2d3d9011c3b38152c7
sunkaixuan <610262374@qq.com>
2016-09-18 18:37:20 +0800

    ok

--- SyntacticSugar/DataType/StringType/StringSugar.cs
+++ SyntacticSugar/DataType/StringType/StringSugar.cs
@@ -28,6 +28,49 @@ namespace SyntacticSugar
             return value.Split(new string[] { splString }, options);
         }
 
+        ///转全角的函数(SBC case)
+        ///任意字符串
+        ///全角字符串
+        ///全角空格为12288，半角空格为32
+        ///其他字符半角(33-126)与全角(65281-65374)的对应关系是：均相差65248
+        public static string ToSBC(this string input)
+        {
+            // 半角转全角：
+            char[] c = input.ToCharArray();
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (c[i] == 32)
+                {
+                    c[i] = (char)12288;
+                    continue;
+                }
+                if (c[i] < 127)
+                    c[i] = (char)(c[i] + 65248);
+            }
+            return new string(c);
+        }
+        /**/
+        /// 转半角的函数(DBC case)
+        ///任意字符串
+        ///半角字符串
+        ///全角空格为12288，半角空格为32
+        ///其他字符半角(33-126)与全角(65281-65374)的对应关系是：均相差65248
+        public static string ToDBC(this string input)
+        {
+            char[] c = input.ToCharArray();
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (c[i] == 12288)
+                {
+                    c[i] = (char)32;
+                    continue;
+                }
+                if (c[i] > 65280 && c[i] < 65375)
+                    c[i] = (char)(c[i] - 65248);
+            }
+            return new string(c);
+        }
+
         /// <summary>
         /// 获取格式化字符串,等同于string.Format
         /// </summary>
