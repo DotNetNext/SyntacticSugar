d062a6dc1614d28327067b3ba3cab71368164b85
sunkaixuan <610262374@qq.com>
2016-11-30 02:24:47 +0800

    2.0.1.3

--- SyntacticSugar/DataType/DateType/DateSugar.cs
+++ SyntacticSugar/DataType/DateType/DateSugar.cs
@@ -11,6 +11,76 @@ namespace SyntacticSugar
     /// </summary>
     public class DateSugar
     {
+        public static string DateStringFromNow(DateTime dt)
+        {
+            TimeSpan span = DateTime.Now - dt;
+            if (span.TotalDays > 60)
+            {
+                return dt.ToShortDateString();
+            }
+            else
+            {
+                if (span.TotalDays > 30)
+                {
+                    return
+                    "1个月前";
+                }
+                else
+                {
+                    if (span.TotalDays > 14)
+                    {
+                        return
+                        "2周前";
+                    }
+                    else
+                    {
+                        if (span.TotalDays > 7)
+                        {
+                            return
+                            "1周前";
+                        }
+                        else
+                        {
+                            if (span.TotalDays > 1)
+                            {
+                                return
+                                string.Format("{0}天前", (int)Math.Floor(span.TotalDays));
+                            }
+                            else
+                            {
+                                if (span.TotalHours > 1)
+                                {
+                                    return
+                                    string.Format("{0}小时前", (int)Math.Floor(span.TotalHours));
+                                }
+                                else
+                                {
+                                    if (span.TotalMinutes > 1)
+                                    {
+                                        return
+                                        string.Format("{0}分钟前", (int)Math.Floor(span.TotalMinutes));
+                                    }
+                                    else
+                                    {
+                                        if (span.TotalSeconds >= 1)
+                                        {
+                                            return
+                                            string.Format("{0}秒前", (int)Math.Floor(span.TotalSeconds));
+                                        }
+                                        else
+                                        {
+                                            return
+                                            "1秒前";
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         /// <summary>
         /// 时间类型
         /// </summary>
